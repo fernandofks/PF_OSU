63b8fedbc11208ad3f8d13c1294fa4f9c9f42e3b
Fernando Sakabe <fernandokenjisakabe@gmail.com>
2019-05-12 14:51:40 -0300

    Add circulo e botão ao arquivo OSU, falta aperto de botão

--- OSU.py
+++ OSU.py
@@ -1,5 +1,6 @@
 import pygame
 from os import path
+import random
 
 # Define some colors
 BLACK = (0, 0, 0)
@@ -12,7 +13,66 @@ DKGREEN = (0, 100, 0)
 img_dir = path.join(path.dirname(__file__), 'img')
 snd_dir = path.join(path.dirname(__file__), "snd")
 fnt_dir = path.join(path.dirname(__file__), "font")
+class circulo(pygame.sprite.Sprite):
+    def __init__(self):
+        
+        # Construtor da classe pai (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+        
+        # Carregando a imagem de fundo.
+        mob_img = pygame.image.load(path.join(img_dir, "circulo.png")).convert()
+        
+        # Diminuindo o tamanho da imagem.
+        self.image = pygame.transform.scale(mob_img, (tamanho, tamanho))
+        
+        # Deixando transparente.
+        self.image.set_colorkey(BLACK)
+        
+        # Detalhes sobre o posicionamento.
+        self.rect = self.image.get_rect()
+        
+        # Sorteia um lugar inicial em x
+        self.rect.centerx = botao.posicao()[0]
+        # Sorteia um lugar inicial em y
+        self.rect.centery = botao.posicao()[1]
+    # Metodo que atualiza a posição da navinha
+    def update1(self):
+        self.rect = self.image.get_rect()
+        self.rect.centerx = botao.posicao()[0]
+        self.rect.centery = botao.posicao()[1]
+        mob_img = pygame.image.load(path.join(img_dir, "circulo.png")).convert()
+        self.image = pygame.transform.scale(mob_img, (tamanho, tamanho))
+        self.image.set_colorkey(BLACK)
 
+
+class Mob(pygame.sprite.Sprite):
+    # Construtor da classe.
+    def __init__(self):
+        
+        # Construtor da classe pai (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+        
+        # Carregando a imagem de fundo.
+        mob_img = pygame.image.load(path.join(img_dir, "botaoup.jpg")).convert()
+        
+        # Diminuindo o tamanho da imagem.
+        self.image = pygame.transform.scale(mob_img, (50, 50))
+        
+        # Deixando transparente.
+        self.image.set_colorkey(BLACK)
+        
+        # Detalhes sobre o posicionamento.
+        self.rect = self.image.get_rect()
+        
+        # Sorteia um lugar inicial em x
+        self.rect.centerx = random.randrange(WIDTH - 100)
+        # Sorteia um lugar inicial em y
+        self.rect.centery = random.randrange(HEIGHT - 100)
+
+        # Melhora a colisão estabelecendo um raio de um circulo
+
+    def posicao(self):
+        return self.rect.centerx, self.rect.centery
 class Player(pygame.sprite.Sprite):
     # Constructor. Pass in the color of the block, and its x and y position
     def __init__(self):
@@ -21,7 +81,8 @@ class Player(pygame.sprite.Sprite):
 
         # Create an image of the player, and fill it with a color.
         # This could also be an image loaded from the disk.
-        self.image = pygame.image.load(path.join(img_dir, "Pointer.png"))
+        self.image = pygame.image.load(path.join(img_dir, "Pointer.png")).convert()
+        self.image.set_colorkey(BLACK)       
 
         # Fetch the rectangle object that has the dimensions of the image
         self.rect = self.image.get_rect()
@@ -42,8 +103,8 @@ class Player(pygame.sprite.Sprite):
 
         # Set the attribute for the top left corner where this object is
         # located
-        self.rect.x = x - 120
-        self.rect.y = y - 120
+        self.rect.centerx = x
+        self.rect.centery = y
 
 pygame.init()
 
@@ -58,11 +119,16 @@ done = False
 
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
-
+WIDTH = pygame.display.Info().current_w
+HEIGHT = pygame.display.Info().current_h
 # This is a list of 'sprites.' Each sprite in the program (there is only 1) is
 # added to this list. The list is managed by a class called 'Group.'
 all_sprites_list = pygame.sprite.Group()
-
+tamanho=200
+botao=Mob()
+timing=circulo()
+all_sprites_list.add(timing)
+all_sprites_list.add(botao)
 # This represents the ball controlled by the player
 player = Player()
 #screen.fill(BLACK)
@@ -74,10 +140,16 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if pygame.sprite.collide_circle(player,botao):
+                    if tamanho>30 and tamanho<70:
+                        botao.kill()
+                        timing.kill()
+    if tamanho>35:
+        tamanho-=5
+        timing.update1()
 
-    # --- Game logic
     all_sprites_list.update()
-
     #limpa a tela depois desenha
     screen.fill(BLACK)
     all_sprites_list.draw(screen)
--- botao.py
+++ botao.py
@@ -5,8 +5,7 @@ from os import path
 
 img_dir = path.join(path.dirname(__file__), 'img')
 
-WIDTH = 480 
-HEIGHT = 600 
+
 FPS = 60 
 
 WHITE = (255, 255, 255)
@@ -15,9 +14,42 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
+class circulo(pygame.sprite.Sprite):
+    def __init__(self):
+        
+        # Construtor da classe pai (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+        
+        # Carregando a imagem de fundo.
+        mob_img = pygame.image.load(path.join(img_dir, "circulo.png")).convert()
+        
+        # Diminuindo o tamanho da imagem.
+        self.image = pygame.transform.scale(mob_img, (tamanho, tamanho))
+        
+        # Deixando transparente.
+        self.image.set_colorkey(BLACK)
+        
+        # Detalhes sobre o posicionamento.
+        self.rect = self.image.get_rect()
+        
+        # Sorteia um lugar inicial em x
+        self.rect.centerx = botao.posicao()[0]
+        # Sorteia um lugar inicial em y
+        self.rect.centery = botao.posicao()[1]
+
+        # Melhora a colisão estabelecendo um raio de um circulo
+        self.radius = int(self.rect.width * .85 / 2)
+    # Metodo que atualiza a posição da navinha
+    def update(self):
+        self.rect = self.image.get_rect()
+        self.rect.centerx = botao.posicao()[0]
+        self.rect.centery = botao.posicao()[1]
+        mob_img = pygame.image.load(path.join(img_dir, "circulo.png")).convert()
+        self.image = pygame.transform.scale(mob_img, (tamanho, tamanho))
+        self.image.set_colorkey(BLACK)
+
 
 class Mob(pygame.sprite.Sprite):
-    
     # Construtor da classe.
     def __init__(self):
         
@@ -25,7 +57,7 @@ class Mob(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         
         # Carregando a imagem de fundo.
-        mob_img = pygame.image.load(path.join(img_dir, "botaoup.png")).convert()
+        mob_img = pygame.image.load(path.join(img_dir, "botaoup.jpg")).convert()
         
         # Diminuindo o tamanho da imagem.
         self.image = pygame.transform.scale(mob_img, (50, 50))
@@ -37,23 +69,19 @@ class Mob(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         
         # Sorteia um lugar inicial em x
-        self.rect.x = random.randrange(WIDTH - self.rect.width)
+        self.rect.centerx = random.randrange(WIDTH - 100)
         # Sorteia um lugar inicial em y
-        self.rect.y = random.randrange(HEIGHT - self.rect.height)
+        self.rect.centery = random.randrange(HEIGHT - 100)
 
         # Melhora a colisão estabelecendo um raio de um circulo
-        self.radius = int(self.rect.width * .85 / 2)
-        
-    # Metodo que atualiza a posição da navinha
-    def update(self):
-        
-        self.rect.x = random.randrange(WIDTH - self.rect.width)
-        self.rect.y = random.randrange(HEIGHT - self.rect.height)
 
     def posicao(self):
-        return self.rect.x, self.rect.y
+        return self.rect.centerx, self.rect.centery
+
 pygame.init()
 pygame.mixer.init()
+WIDTH = pygame.display.Info().current_w
+HEIGHT = pygame.display.Info().current_h
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("OSU2")
 clock = pygame.time.Clock()
@@ -62,7 +90,10 @@ background_rect = background.get_rect()
 all_sprites = pygame.sprite.Group()
 botoes = pygame.sprite.Group()
 botao = Mob()
+tamanho=200
+timing=circulo()
 all_sprites.add(botao)
+all_sprites.add(timing)
 botoes.add(botao)
 
 try:
@@ -83,11 +114,16 @@ try:
             
             # Verifica se apertou alguma tecla.
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if pygame.mouse.get_pos()[0]<botao.posicao()[0]+50 and pygame.mouse.get_pos()[0]>botao.posicao()[0] and pygame.mouse.get_pos()[1]>botao.posicao()[1] and pygame.mouse.get_pos()[1]<botao.posicao()[1]+50:
-                    botao.update()
+                if pygame.mouse.get_pos()[0]<botao.posicao()[0]+25 and pygame.mouse.get_pos()[0]>botao.posicao()[0]-25 and pygame.mouse.get_pos()[1]>botao.posicao()[1]-25 and pygame.mouse.get_pos()[1]<botao.posicao()[1]+25:
+                    if tamanho>30 and tamanho<70:
+                        botao.kill()
+                        timing.kill()
+        if tamanho>35:
+            tamanho-=3
+            timing.update()
+
+        
         # Depois de processar os eventos.
-    
- 
         # A cada loop, redesenha o fundo e os sprites
         screen.fill(BLACK)
         screen.blit(background, background_rect)
